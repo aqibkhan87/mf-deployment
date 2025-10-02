26d40e30b9b8e8c28bc660afceaf0ef990c8233e
aqibkhan87 <142089072+aqibkhan87@users.noreply.github.com>
2025-10-02 19:46:47 +0530

    lint fixes committed

--- packages/store/src/store/authStore.js
+++ packages/store/src/store/authStore.js
@@ -3,13 +3,24 @@ import { create } from "zustand";
 
 console.log("Inside Store");
 export const useAuthStore = create((set, get) => ({
-  user: JSON.parse(localStorage.getItem('user')) || {},
-  address: JSON.parse(localStorage.getItem('address')) || "",
+  user: JSON.parse(localStorage.getItem("user")) || {},
+  address: JSON.parse(localStorage.getItem("address")) || "",
   theme: "light",
   settings: {},
 
   setUser: (user) => set({ user }),
-  setNewAddress: (address) => set(address),
+  setNewAddress: (address) => {
+    const {
+      name,
+      address: addressField,
+      locality,
+      city,
+      pincode,
+      mobile,
+    } = address;
+    let updatedAddress = `${name}, ${addressField}, ${locality}, ${city}, ${pincode}, ${mobile}`;
+    set(updatedAddress);
+  },
   logout: () => set({ user: null }),
   // toggleTheme: () =>
   //   set((s) => ({ theme: s.theme === "light" ? "dark" : "light" })),
